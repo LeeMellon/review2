0daf06604ef183740792860bb0fdca126cb6ca92
Ian Goodrich <iangoodrich@hotmail.com>
2018-01-12 15:08:17 -0800

    add result function, and completed readme.

--- README.md
+++ README.md
@@ -1,37 +1,34 @@
-#_{Voit-cough-Programming-Language-Survey}_
+# Voit-cough-Programming-Language-Survey
 
-#### _{A web app designed to help the user decide which programming language best suits them. Also may tell you if you're a Cylon or a Replicant.}_
+####  A web app designed to help the user decide which programming language best suits them. Also may tell you if you're a Cylon or a Replicant.
 
-#### _{By Ian Goodrich}_
+#### By Ian Goodrich
 
 ## Description
 
-_{Presents the user with a series of questions to help determine which coding language and as such which area of the larger field of professional coding  they may want to explore. To this end each available response is weighted (based on what each question says about the respondents propensity or proclivity for aesthetic appreciation, social interaction, and front or backend work)to reflect which language is most and least represented by a given answer. In order to avoid simple "black-or-white" results, this "weighting" is done along a spectrum of 1 to 0 with .25 increments. }_
+ Presents the user with a series of questions to help determine which coding language and as such which area of the larger field of professional coding  they may want to explore. To this end each available response is weighted (based on what each question says about the respondents propensity or proclivity for aesthetic appreciation, social interaction, and front or backend work)to reflect which language is most and least represented by a given answer. In order to avoid simple "black-or-white" results, this "weighting" is done along a spectrum of 1 to 0 with .25 increments.
 
 ## Setup/Installation Requirements
 
-* _This is a great place_
-* _to list setup instructions_
-* _in a simple_
-* _easy-to-understand_
-* _format_
+* Double click on, or drag the index.html file to your favorite web browser.
+* Input the requested information.
+* Respond to each question as it appears.
+* Your results will be calculated immediately upon completion.
 
-_{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_
 
 ## Known Bugs
 
-_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
-
+_
 ## Support and contact details
 
-_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+If for any reason you need to contact me please email at iangoodrich@hotmail.com
 
 ## Technologies Used
 
-_{Tell me about the languages and tools you used to create this app. Assume that I know you probably used HTML and CSS. If you did something really cool using only HTML, point that out.}_
+The visual effects and backend logic are written in JavaScript and JQuery.
 
 ### License
 
-*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+This web app is free to use under GPL standards.
 
-Copyright (c) 2016 **_{List of contributors or company name}_**
+Copyright (c) 2018 by Ian Goodrich
--- js/scripts.js
+++ js/scripts.js
@@ -4,91 +4,124 @@ $(function() {
   var php = 0;
   var java = 0;
   var css = 0;
+  var winner = "nothing";
+  console.log("1" + winner);
+
+  var result = (function(c, ruby, php, java, css) {
+    if (c > ruby && c > php && c > java && c > css) {
+      var winner = "C#";
+      console.log("2" + winner);
+    } else if (ruby > c && ruby > php && ruby > java && ruby > css) {
+      var winner = "Ruby";
+      console.log("3" + winner);
+
+    } else if (php > c && php > ruby && php > java && php > css) {
+      var winner = "php";
+      console.log("4" + winner);
+
+    } else if (java > c && java > ruby && java > css && java > css) {
+      var winner = "JavaScript";
+      console.log("5" + winner);
+
+    } else if (css > c && css > ruby && css > php && css > java) {
+      var winner = "CSS";
+    }
+    console.log("6" + winner);
+    return winner;
+  });
+
+
+
+
+
 
   $("#calf").change(function(event) {
     var calf = $("input:radio[name=calf]:checked").val();
     event.preventDefault();
     $("#calfDiv").fadeOut(900, "swing", "complete");
-    $("#jarDiv").delay(910).fadeIn(900, "swing");
+    $("#jarDiv").delay(950).fadeIn(900, "swing");
     console.log(calf);
 
-    if (calf = "a") {
+    if (calf === "a") {
       c += .5, ruby += 1.0, php += 0.0, java += .75, css += .25;
-    } else if (calf = "b") {
+    } else if (calf === "b") {
       c += 1, ruby += 0, php += 1, java += .5, css += .5;
-    } else if (calf = "c") {
+    } else if (calf === "c") {
       c += 0, ruby += 1, php += .5, java += .25, css += 1;
     }
     console.log(c, ruby, php, java, css);
-
   });
 
   $("#jar").change(function(event) {
     var jar = $("input:radio[name=jar]:checked").val();
     event.preventDefault();
     $("#jarDiv").fadeOut(900, "swing", "complete");
-    $("#waspDiv").delay(910).fadeIn(900, "swing");
+    $("#waspDiv").delay(950).fadeIn(900, "swing");
     console.log(jar);
 
-    if (jar = "a") {
+    if (jar === "a") {
       c += .5, ruby += 1.0, php += 0.0, java += .75, css += .25;
-    } else if (jar = "b") {
+    } else if (jar === "b") {
       c += 1, ruby += 0, php += 1, java += .5, css += .5;
-    } else if (jar = "c") {
+    } else if (jar === "c") {
       c += 0, ruby += 1, php += .5, java += .25, css += 1;
     }
     console.log(c, ruby, php, java, css);
+
   });
 
   $("#wasp").change(function(event) {
     var wasp = $("input:radio[name=wasp]:checked").val();
     event.preventDefault();
     $("#waspDiv").fadeOut(900, "swing", "complete");
-    $("#dogDiv").delay(910).fadeIn(900, "swing");
+    $("#dogDiv").delay(950).fadeIn(900, "swing");
     console.log(wasp);
 
-    if (wasp = "a") {
+    if (wasp === "a") {
       c += .5, ruby += 1.0, php += 0.0, java += .75, css += .25;
-    } else if (wasp = "b") {
+    } else if (wasp === "b") {
       c += 1, ruby += 0, php += 1, java += .5, css += .5;
-    } else if (wasp = "c") {
+    } else if (wasp === "c") {
       c += 0, ruby += 1, php += .5, java += .25, css += 1;
     }
     console.log(c, ruby, php, java, css);
+
   });
 
   $("#dog").change(function(event) {
     var dog = $("input:radio[name=dog]:checked").val();
     event.preventDefault();
     $("#dogDiv").fadeOut(900, "swing", "complete");
-    $("#tortDiv").delay(910).fadeIn(900, "swing");
+    $("#tortDiv").delay(950).fadeIn(900, "swing");
     console.log(dog);
 
-    if (dog = "a") {
+    if (dog === "a") {
       c += .5, ruby += 1.0, php += 0.0, java += .75, css += .25;
-    } else if (dog = "b") {
+    } else if (dog === "b") {
       c += 1, ruby += 0, php += 1, java += .5, css += .5;
-    } else if (dog = "c") {
+    } else if (dog === "c") {
       c += 0, ruby += 1, php += .5, java += .25, css += 1;
     }
     console.log(c, ruby, php, java, css);
+
   });
 
   $("#tortoise").change(function(event) {
     var tort = $("input:radio[name=tortoise]:checked").val();
     event.preventDefault();
     $("#tortDiv").fadeOut(900, "swing", "complete");
-    $("#resultDiv").delay(910).fadeIn(900, "swing");
+    $("#resultDiv").delay(950).fadeIn(900, "swing");
     console.log(tort);
 
-    if (tort = "a") {
+    if (tort === "a") {
       c += .5, ruby += 1.0, php += 0.0, java += .75, css += .25;
-    } else if (tort = "b") {
+    } else if (tort === "b") {
       c += 1, ruby += 0, php += 1, java += .5, css += .5;
-    } else if (tort = "c") {
+    } else if (tort === "c") {
       c += 0, ruby += 1, php += .5, java += .25, css += 1;
     }
     console.log(c, ruby, php, java, css);
+    result(c, ruby, php, java, css);
   });
 
 
